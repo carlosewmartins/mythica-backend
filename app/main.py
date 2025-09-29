8fb6d9f4a0aa52ce5542a1215492e882b25a2b93
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-09-29 15:32:49 -0300

    refactor: configura CORS

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import auth, personagem, campanha, historico
 from app.core.config import settings
 
@@ -7,7 +8,23 @@ app = FastAPI(
     version=settings.VERSION
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[settings.CORS],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def root():
+    return {
+        "message": f"{settings.PROJECT_NAME} API está rodando!",
+        "version": settings.VERSION
+    }
+
 app.include_router(auth.router, prefix="/api")
 app.include_router(personagem.router, prefix="/api")
 app.include_router(campanha.router, prefix="/api")
 app.include_router(historico.router, prefix="/api")
+
