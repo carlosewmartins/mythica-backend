083070d4e299c3c296c9acab2e6a274b171bda83
Carlos Eduardo Wille Martins <tekaxis.dev@gmail.com>
2025-09-29 11:00:26 -0300

    chore: adiciona rotas para personagem ao swagger

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-from app.api.routes import auth
-app = FastAPI()
+from app.api.routes import auth, personagem
+from app.core.config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.VERSION
+)
+
 app.include_router(auth.router, prefix="/api")
-@app.get("/api/health")
-def health():
-    return {"status": "ok"}
-    
+app.include_router(personagem.router, prefix="/api")
